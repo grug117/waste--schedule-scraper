8bc79755a45ba6a8e18b5925c6ba240facce5f31
grug117 <gregford117@live.com>
2023-01-11 23:52:42 +0000

    fix: bundle script error

--- bundle.sh
+++ bundle.sh
@@ -10,7 +10,7 @@ conda activate $1
 conda install --file requirements.txt
 
 base_env_site_packages=$(python -c "import site; print(site.getsitepackages()[0])")
-working_dir = $(pwd)
+working_dir=$(pwd)
 
 cd $base_env_site_packages
 zip -r dependencies.zip *
