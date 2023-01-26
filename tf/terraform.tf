131bfd82c273d3ccf17e4b2d9631c0de22696e87
Greg Ford <gregford@Gregs-MacBook-Pro.local>
2023-01-26 22:44:48 +0000

    gave up trying to pass through the shortlived session token from the gh actions role

--- tf/terraform.tf
+++ tf/terraform.tf
@@ -18,7 +18,4 @@ terraform {
 
 provider "aws" {
   region     = "eu-west-1"
-  access_key = "${var.access_key_id}"
-  secret_key = "${var.secret_access_key}"
-  token      = "${var.session_token}"
 }
--- tf/variables.tf
+++ tf/variables.tf
@@ -2,15 +2,3 @@ variable "environment" {
   type    = string
   default = "dev"
 }
-
-variable "access_key_id" {
-  type = string
-}
-
-variable "secret_access_key" {
-  type = string
-}
-
-variable "session_token" {
-  type = string
-}
